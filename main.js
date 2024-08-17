bc294240fe6ccf5f4d995b9d8167d2271d8f1634
Igor Bezhinarov <igorbezhinarov@yandex.ru>
2024-08-17 20:22:18 +0700

    brands section has be created

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ import 'swiper/css/bundle';
 
 import './sass/style.scss';
 
+//слайдер для промо блока
 const promoSlider = new Swiper('.promo__slider', {
     slidesPerView: 1,
     spaceBetween: 40,
@@ -13,6 +14,19 @@ const promoSlider = new Swiper('.promo__slider', {
         nextEl: '.promo__arrow_next',
         prevEl: '.promo__arrow_prev',
     },
+    autoplay: {
+        delay: 6000,
+        disableOnInteraction: false,
+        pauseOnMouseEnter: true,
+    },
+});
+
+//слайдер для блока бренды
+const brandSlider = new Swiper('.brand__slider', {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    speed: 800,
+    loop: false,
     autoplay: {
         delay: 6000,
         disableOnInteraction: false,
