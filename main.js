57e1a4c94b48c1154eabe3dcd91d5d92d9a29c15
Igor Bezhinarov <igorbezhinarov@yandex.ru>
2024-08-17 20:04:34 +0700

    promo slider has be created

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
-import Swiper from 'swiper';
+import Swiper from 'swiper/bundle';
+// import styles bundle
+import 'swiper/css/bundle';
 
-import 'swiper/css';
 import './sass/style.scss';
+
+const promoSlider = new Swiper('.promo__slider', {
+    slidesPerView: 1,
+    spaceBetween: 40,
+    speed: 800,
+    loop: false,
+    navigation: {
+        nextEl: '.promo__arrow_next',
+        prevEl: '.promo__arrow_prev',
+    },
+    autoplay: {
+        delay: 6000,
+        disableOnInteraction: false,
+        pauseOnMouseEnter: true,
+    },
+});
